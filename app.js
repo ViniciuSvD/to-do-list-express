c7acfffb1608887be0fd16aa5cd92b62e14c07c6
Vinícius Melo <vpena.dev@gmail.com>
2022-07-01 21:47:03 -0300

    Criação de telas e integrando-as com seus respectivos métodos

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ const checklistsRouter = require('./src/routes/checklist')
 const rootRouter = require('./src/routes/index')
 require('./config/database')
 
+
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('views', path.join(__dirname, 'src/views'))
 app.set('view engine', 'ejs')
